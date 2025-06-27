bcd8e9382c7511d98346f7e47f35e893da8403af
Lutttfi <lutfiofficial149@gmail.com>
2025-06-27 08:54:16 +0700

    yer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector(".title");
-const text = `For Yerrr`.split("");
+const text = `FLOWERS FOR YERRR`.split("");
 
 // Create container for better responsive layout
 title.style.display = "flex";
