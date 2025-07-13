50fadb439fa9f1c29d9b6592f52cbf2e4c4485e0
Lutttfi <lutfiofficial149@gmail.com>
2025-07-13 21:26:53 +0700

    berubah

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector(".title");
-const text = `FOR YERRR`.split("");
+const text = `I HAVE SOMETHING FOR U`.split("");
 
 // Create container for better responsive layout
 title.style.display = "flex";
